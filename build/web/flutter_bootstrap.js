28862b1dabb2108e768a2811f11f481aca8b5b17
Samir <samir@example.com>
2026-07-09 13:57:06 +0200

    rebuild web app with default canvaskit renderer

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1983644777" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "85329610" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
